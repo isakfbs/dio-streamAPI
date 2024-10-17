ede735928216374b80b1b372303cf8e7f4630262
Isak Silva <isakfreires98@gmail.com>
2024-10-17 20:51:46 -0300

    Implementa desafio 03 ao 06

--- src/steam_api/desafios/ShowNumericOrderList.java
+++ src/steam_api/desafios/ShowNumericOrderList.java
@@ -17,6 +17,21 @@ public class ShowNumericOrderList {
         System.out.println(numeros.stream().filter(n -> n%2==0).reduce(0, Integer::sum));
         System.out.println();
 
+    //Com a ajuda da Stream API, verifique se todos os números da lista são
+    // positivos e exiba o resultado no console.
+        System.out.println(numeros.stream().allMatch(n -> n>0));
+        System.out.println();
 
+    //Utilize a Stream API para remover os valores ímpares da lista
+    // e imprima a lista resultante no console.
+        numeros.stream().filter(n -> n%2==0).forEach(System.out::println);
+        System.out.println();
+
+    //Com a Stream API, calcule a média dos números maiores que 5 e exiba o resultado no console.
+        System.out.println(""+ numeros.stream().filter(n -> n > 5).mapToDouble(n -> n).average());
+        System.out.println();
+    //Utilize a Stream API para verificar se a lista contém algum número maior que 10 e exiba o resultado no console.
+        System.out.println(numeros.stream().anyMatch(n -> n > 10));
+        System.out.println();
     }
 }
